caba8e3ec4f44a0b399d28e6209c734d1078f0d8
KEN ROE <shdnrnjs@gmail.com>
2023-10-19 17:32:02 +0900

    리액트와 타입스크립트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+
+  // state 타입은  만들ㄹ 때 자동으로 할당되는데, 나중에 변화할 수 있는 경우 미리 지정
+  const [user, setUser]  = useState<string | null>('');
+
+  // type assertion 문법 사용할 때 = as사용 <>는 컴포넌트로 오해할 수 있어서 리액트에서 사용 안 함. as 키워드 또한 타입이 100% 확실할 때만 사용.
+  // 타입스크립트 쓴다고 리액트 개발ㄹ방식이 달라지는게 아니라 함수 변수 정의부분 타입지정을 할 수 있다는 것만 달라질 뿐.
+  let code: any = 123;
+  
+
+  let box: JSX.Element = <div></div>
+  let button: JSX.Element = <button></button>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/Container.tsx
@@ -0,0 +1,13 @@
+// type ContainerProps = {
+//   a: JSX.IntrinsicElements['h4'];
+// }
+
+// const Container = (props: ContainerProps) => {
+//   return (
+//     <div>
+//       {props.a}
+//     </div>
+//   )
+// }
+
+// export default Container;
