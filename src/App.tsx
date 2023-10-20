a4ea1520e37d2d1928ab5e6bed2d202e89b18553
KEN ROE <shdnrnjs@gmail.com>
2023-10-20 09:17:45 +0900

    redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector, } from 'react-redux/es/exports';
+import { Dispatch } from 'redux';
+import { RootState, increment } from '.';
 
 function App(): JSX.Element {
 
+  // 전통방식 - redux에서 state 꺼내오기
+  const pick = useSelector((state: RootState) => state); //useSelector의 파라미터 타입을 지정해줘야 하는데, 미리 export 해두면 편함
+  const dispatch: Dispatch = useDispatch();
+
+  // 신규방식
+  const pick2 = useSelector((state: RootState) => state) // useSelector 함수 사용하여 state 꺼냄. 콜백함수를 파라미터로 넣는다. 함수의 첫 파라미터는 항상 state가 됨.
+  const dispatch2 = useDispatch(); // useDispatch 함수를 쓰면 쉽게 수정요청 가능
+
+
   // state 타입은  만들ㄹ 때 자동으로 할당되는데, 나중에 변화할 수 있는 경우 미리 지정
-  const [user, setUser]  = useState<string | null>('');
+  const [user, setUser] = useState<string | null>('');
 
   // type assertion 문법 사용할 때 = as사용 <>는 컴포넌트로 오해할 수 있어서 리액트에서 사용 안 함. as 키워드 또한 타입이 100% 확실할 때만 사용.
   // 타입스크립트 쓴다고 리액트 개발ㄹ방식이 달라지는게 아니라 함수 변수 정의부분 타입지정을 할 수 있다는 것만 달라질 뿐.
   let code: any = 123;
-  
+
 
   let box: JSX.Element = <div></div>
   let button: JSX.Element = <button></button>
 
   return (
     <div className="App">
-      
+      {pick2.counter1.count}
+      <button onClick={() => {dispatch({type : 'increase'})}}>버튼</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';;
+import { createStore } from 'redux';
+import { createSlice, configureStore, PayloadAction } from '@reduxjs/toolkit';
+import exp from 'constants';
+
+
+// 전통방식
+interface Counter {
+  count: number
+}
+
+const init: Counter = { count: 0 };
+
+function reducer(state = init, action: any) {
+  if (action.type === 'increase') {
+    return { ...state, count: state.count + 1 }
+  } else if (action.type === 'decrease') {
+    return { ...state, count: state.count - 1 }
+  } else {
+    return init;
+  }
+}
+const store = createStore(reducer);
+
+// export type RootState = ReturnType<typeof store.getState>
+
+// 새로운 방식
+const init2  = { count: 0, user: 'kim'}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: init2,
+  reducers: {
+    increment (state) {
+      state.count += 1
+    },
+    decrement (state) {
+      state.count -= 1
+    },
+    incrementByAmount (state, action: PayloadAction<number>) {
+      state.count += action.payload
+    }
+  }
+})
+
+let store2 = configureStore({
+  reducer: {
+    counter1: counterSlice.reducer
+  }
+})
+
+// state타입을 export 해두는 건데 나중에 쓸 일 있음
+export type RootState = ReturnType<typeof store2.getState>
+
+// 수정방법 만든거 export
+export let {increment, decrement, incrementByAmount} = counterSlice.actions
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store2}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
